ac2b83da16fa1e59cfae69af7df1ac25b14b1eb1
Rider <riderjensen@gmail.com>
2019-02-25 20:30:50 -0700

    Home container

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Auxil from '../../hoc/Auxil/Auxil';
+import MainCta from '../../components/Home/Header/Heads';
+
+export default class Home extends Component {
+	render() {
+		return (
+			<Auxil>
+				<MainCta />
+
+			</Auxil>
+		)
+	}
+}

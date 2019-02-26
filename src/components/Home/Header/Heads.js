14b365e7a40b68aca8b61e35e228f0f8e7ddebf5
Rider <riderjensen@gmail.com>
2019-02-25 20:03:41 -0700

    Add heads as main CTA

--- src/components/Home/Header/Heads.js
+++ src/components/Home/Header/Heads.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import classes from './Heads.css';
 
+import handshake from '../../../assets/images/handshake.jpg';
+
 export default function Header() {
 	return (
-		<div className={classes.Heads}>
-		</div>
+		<div className={classes.Heads} style={{
+			backgroundImage: `url(${handshake})`,
+		}}>
+			<div className={classes.Overlay}></div>
+			<div className={classes.Content}>
+				<div className={classes.ContentBox}>
+					<h1>Platform Creator</h1>
+					<p>Create the perfect political campaign website in just 10 minutes.</p>
+				</div>
+			</div>
+		</div >
 	)
 }

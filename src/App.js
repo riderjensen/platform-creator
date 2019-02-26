db87df55722811bed17377889800a142b01c4591
Rider <riderjensen@gmail.com>
2019-02-25 21:02:27 -0700

    Import bootstrap, tweak home container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import classes from './App.css';
 
 import Home from './containers/Home/Home';
 
 class App extends Component {
 	render() {
 		return (
-			<div className={classes.App}>
-				<Switch>
-					{/* <Route path="/checkout" component={Heads} /> */}
-					<Route path="/" component={Home} />
-				</Switch>
-			</div>
+			<Switch>
+				{/* <Route path="/checkout" component={Heads} /> */}
+				<Route path="/" component={Home} />
+			</Switch>
 		);
 	}
 }
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 
 import Auxil from '../../hoc/Auxil/Auxil';
 import MainCta from '../../components/Home/Header/Heads';
+import Input from '../../components/UI/Input/Input';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<Auxil>
 				<MainCta />
-
+				<div className="container">
+					<div className="row">
+						<div className="col-xs-12 col-sm-6">
+							<Input inputType="text" inputSetters={{
+								placeholder: 'Name'
+							}} />
+						</div>
+						<div className="col-xs-12 col-sm-6">
+							<Input inputType="text" inputSetters={{
+								placeholder: 'Name'
+							}} />
+						</div>
+					</div>
+				</div>
 			</Auxil>
 		)
 	}

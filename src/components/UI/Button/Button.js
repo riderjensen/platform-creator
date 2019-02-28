f0e192b0584fc3c4647c833e99ce061bd48f1531
Rider <riderjensen@gmail.com>
2019-02-27 19:52:26 -0700

    Fix button js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,6 @@ import classes from './Button.css';
 
 export default function Button(props) {
 	return (
-		<a className={classes.Button} href={props.click}>{props.children}</a>
+		<a className={classes.Button} href={props.click} onClick={props.onClick}>{props.children}</a>
 	)
 }

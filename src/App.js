78f3a8a1a65766d9f9e4c4054294c84afd554b05
Rider <riderjensen@gmail.com>
2019-02-27 20:03:29 -0700

    add route for path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home/Home';
+import Questions from './containers/Questions/Questions';
 
 class App extends Component {
 	render() {
 		return (
 			<Switch>
 				{/* <Route path="/checkout" component={Heads} /> */}
+				<Route path="/questions" component={Questions} />
 				<Route path="/" component={Home} />
 			</Switch>
 		);

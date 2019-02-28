3568b4d6c0cdc7335789abf2099c2f9d8c5f3543
Rider <riderjensen@gmail.com>
2019-02-27 19:52:43 -0700

    Inputs added to main page

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,6 +3,6 @@ import classes from './Input.css'
 
 export default function Input(props) {
 	return (
-		<input className={classes.Input} {...props.inputSetters} />
+		<input className={classes.Input} onChange={(e) => props.changeHandler(e.target.value, e.target.name)} {...props.inputSetters} />
 	)
 }
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react'
 
-import Auxil from '../../hoc/Auxil/Auxil';
 import MainCta from '../../components/Home/Header/Heads';
 import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
 
 export default class Home extends Component {
+	state = {
+		inputs: {
+			name: {
+				placeholder: 'Name',
+				name: 'name',
+				required: true,
+				type: 'text',
+				value: '',
+			},
+			office: {
+				placeholder: 'What position are you running for?',
+				name: 'office',
+				required: true,
+				type: 'text',
+				value: '',
+			}
+		}
+
+	}
+
+	handleChange = (val, name) => {
+		const newState = { ...this.state.inputs };
+		newState[name].value = val;
+		this.setState({
+			inputs: newState
+		})
+	}
+
+	continueHandler = () => {
+		console.log(this.state.inputs)
+	}
+
 	render() {
 		return (
-			<Auxil>
-				<MainCta />
+			<MainCta>
 				<div className="container">
-					<div className="row">
-						<div className="col-xs-12 col-sm-6">
-							<Input inputSetters={{
-								placeholder: 'Name',
-								type: 'text'
-							}} />
+					{Object.keys(this.state.inputs).map((key, index) => {
+						return <div key={index} className="row">
+							<div className="col-xs-12 col-sm-6">
+								<Input changeHandler={this.handleChange} inputSetters={this.state.inputs[key]} />
+							</div>
+							<div className="col-xs-12 col-sm-6"></div>
 						</div>
-						<div className="col-xs-12 col-sm-6">
-							<Input inputSetters={{
-								placeholder: 'Name',
-								type: 'text'
-							}} />
-						</div>
-					</div>
+					}
+					)}
+					<p><Button onClick={this.continueHandler}>Continue</Button></p>
 				</div>
-			</Auxil>
+			</MainCta>
+
 		)
 	}
 }

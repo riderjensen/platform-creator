043e5878e946bfaeb670e2f25a0e19aea3afd944
Rider <riderjensen@gmail.com>
2019-03-03 12:39:02 -0700

    Site class creation

---
+++ src/containers/Site/Site.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Site extends Component {
+	render() {
+		return (
+			<div className="container">
+				<div className="col-xs-12">
+					<h1>Site router</h1>
+				</div>
+			</div>
+		)
+	}
+}
